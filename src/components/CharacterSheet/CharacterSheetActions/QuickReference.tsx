f13a59b0f92ec87d28b642ffbae578de1949e426
andrewettensohn <andrewettensohn@gmail.com>
2021-11-27 18:27:08 -0500

    Fix quick reference not loading

--- src/components/CharacterSheet/CharacterSheetActions/QuickReference.tsx
+++ src/components/CharacterSheet/CharacterSheetActions/QuickReference.tsx
@@ -3,19 +3,11 @@ import React from "react";
 import { useAppStyles } from "../../AppStyles";
 
 export const QuickReference = () => {
-    const [isLoading, setIsLoading] = React.useState(true);
     const classes = useAppStyles();
 
-    return !isLoading
-        ?
+    return (
         <div>
             <img src="images/QuickReference1.png" className={classes.imgAction} />
             <img src="images/QuickReference2.png" className={classes.imgAction} />
-        </div>
-        :
-        <Grid container justifyContent="center">
-            <Grid item>
-                <CircularProgress color="secondary" className={classes.centerScreen} />
-            </Grid>
-        </Grid>
+        </div>)
 }
